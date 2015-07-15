59cb22dd92660c39ebc484f0e16db7d57af29750
Brinly Taylor <uberlaggydarwin@gmail.com>
2015-07-15 18:41:28 +0930

    odroidc1: don't override the build prop : PRODUCT_NAME

--- cm.mk
+++ cm.mk
@@ -17,8 +17,5 @@ $(call inherit-product, device/hardkernel/odroidc1/full_odroidc1.mk)
 # Inherit CM common Phone stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-# Build fingerprints
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=C1
-
 PRODUCT_NAME := cm_odroidc1
 PRODUCT_DEVICE := odroidc1
